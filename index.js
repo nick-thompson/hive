6f6ff4c84f4a9684d35b146cabb2930164b7f4b2
Nick Thompson <ncthom91@gmail.com>
2015-05-07 17:29:20 -0700

    Add more intensive work for benchmarking

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var Hive = require('./lib/Hive');
 var path = require('path');
 
 var expr = path.resolve('./test/fixtures/expr.js');
-Hive.take(expr, function(err, res) {
-  console.log(res);
-});
+
+for (var i = 0; i < 128; i++) {
+  (function(j) {
+    Hive.take(expr, function(err, res) {
+      console.log(j + ':', res);
+    });
+  })(i);
+}
--- test/fixtures/expr.js
+++ test/fixtures/expr.js
@@ -1,1 +1,8 @@
-7 + 9
+function fib(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib(35);
