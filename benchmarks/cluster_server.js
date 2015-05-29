8907e5a04d08fb908e40d20c99e98717d6e0d2e3
Nick Thompson <ncthom91@gmail.com>
2015-05-29 16:20:22 -0700

    Magic number explanation

--- benchmarks/cluster_server.js
+++ benchmarks/cluster_server.js
@@ -2,6 +2,8 @@ var cluster = require('cluster');
 var net = require('net');
 
 if (cluster.isMaster) {
+  // The constraint of 4 processes allows a better comparison to the Hive
+  // threading model, wherein the default libuv thread pool size is 4.
   for (var i = 0; i < 4; i++) {
     cluster.fork();
   }
