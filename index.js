016d9af200651a2a1db6e7a9a6c19690a5a94eb8
Nick Thompson <ncthom91@gmail.com>
2015-06-16 15:23:04 -0700

    Usage invariants

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var addon = require('bindings')('addon.node');
+var assert = require('assert');
+
+var initialized = false;
 
 function init(script) {
+  assert.equal(false, initialized, 'Hive.init must be called only once.');
   script = script || "";
+  initialized = true;
   return addon.init(script);
 }
 
 function eval(expr, callback) {
+  assert.equal(true, initialized, 'Hive.init must be called before Hive.eval.');
   return addon.eval(expr, function(err, res, latency) {
     if (err) return callback(err);
     if (typeof res === 'undefined') return callback(null, undefined, latency);
